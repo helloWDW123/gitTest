2e871a74e45ac593a42ade803ac2ca569c53db60
王大王 <hellofuture56@163.com>
2017-10-12 17:46:20 +0800

    frist commit

---
+++ gittest/gittest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gittest
+//
+//  Created by 王大王 on 2017/10/12.
+//  Copyright © 2017年 wdw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gittest/gittest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gittest
+//
+//  Created by 王大王 on 2017/10/12.
+//  Copyright © 2017年 wdw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
